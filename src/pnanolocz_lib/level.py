bb8b65bf9318a142d6cf6bdb1063b353118cb884
Daniel/Eddie Rollins <69976751+derollins@users.noreply.github.com>
2025-07-18 17:53:59 +0100

    Move RankWarning import to the top of the module.

--- src/pnanolocz_lib/level.py
+++ src/pnanolocz_lib/level.py
@@ -46,6 +46,7 @@ This module is part of the pNanoLocz-Lib Python library for AFM analysis.
 """
 
 import numpy as np
+from numpy.polynomial.polyutils import RankWarning
 import warnings
 from typing import Optional, Literal
 from scipy.optimize import curve_fit
@@ -107,7 +108,6 @@ def level_plane(
     standardized_columns = (column_indices - col_centroid) / col_scale
 
     with warnings.catch_warnings():
-        from numpy.polynomial.polyutils import RankWarning
         warnings.simplefilter("ignore", RankWarning)
         # polyfit(..., polyx) with centering ⇒ same as MATLAB’s p, ~, mu
         x_coeffs = np.polyfit(
@@ -145,7 +145,6 @@ def level_plane(
     standardized_rows = (row_indices - row_centroid) / row_scale
 
     with warnings.catch_warnings():
-        from numpy.polynomial.polyutils import RankWarning
         warnings.simplefilter("ignore", RankWarning)
         y_coeffs = np.polyfit(
             standardized_rows,
@@ -206,7 +205,6 @@ def level_line(
                 standardized_cols = (col_indices - centroid_col) / scale_col
 
                 with warnings.catch_warnings():
-                    from numpy.polynomial.polyutils import RankWarning
                     warnings.simplefilter("ignore", RankWarning)
                     row_coeffs = np.polyfit(
                         standardized_cols, row_values, polyx
@@ -241,7 +239,6 @@ def level_line(
                 standardized_rows = (row_indices - centroid_row) / scale_row
 
                 with warnings.catch_warnings():
-                    from numpy.polynomial.polyutils import RankWarning
                     warnings.simplefilter("ignore", RankWarning)
                     col_coeffs = np.polyfit(
                         standardized_rows, col_values, polyy
