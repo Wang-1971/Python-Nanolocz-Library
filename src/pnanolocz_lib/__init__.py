f907778a6359f6a06be6bf5af7f329fbf78bb2ec
Daniel Eddie Rollins <dan.rollins1@gmail.com>
2025-11-03 20:51:18 +0000

    add automatic versioning and keyword tags

--- src/pnanolocz_lib/__init__.py
+++ src/pnanolocz_lib/__init__.py
@@ -1,1 +1,8 @@
 """pnanolocz_lib: AFM and HS-AFM data analysis tools."""
+
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("pnanolocz_lib")
+except PackageNotFoundError:
+    __version__ = "0.0.0"
